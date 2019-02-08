29bf6b9d184f02199dd6a4ac4b3a82cc02e5c600
joph <johannes.schmidt@boku.ac.at>
2019-02-08 14:44:43 +0100

    First commit of basic course files

---
+++ ml_classes.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  6 11:15:16 2019
+
+@author: joph
+"""
+
+import numpy as np
+import os
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+#from pandas_datareader import data
+import json
+import urllib.request
+import math
+import statsmodels.api as sm
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LeakyReLU
+from keras.layers import LSTM
+from keras.optimizers import adam
+
+from keras.utils import plot_model
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+import pydot
+
+class Function_data_no_ts:
+    
+    def __init__(self,n,upper,max_elem,model,f,lags):
+        
+        self.model=model
+        self.upper=upper
+        self.max_elem=max_elem
+        self.f=f
+        self.lags=lags
+        x=np.random.random(n)*upper
+        x=np.sort(x)
+        y=self.function(x)
+        self.dataset= [*zip(*[x,y])]
+        
+        
+        lagged=np.zeros((len(x)-self.lags,self.lags))
+
+        for i in range(0,len(x)-lags):
+            lagged[i,:]=x[i:i+lags]
+            
+        y=y[0:(len(x)-lags)]
+        y.shape=(len(x)-lags,1)
+        self.dataset = np.append(lagged,y,1)
+        
+        print(len(self.dataset))
+        
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.dataset_scaled = self.scaler.fit_transform(self.dataset)
+        
+        self.dataset_scaled_short = self.dataset_scaled[0:max_elem,:]
+        self.dataset_out_of_range = self.dataset_scaled[max_elem:self.dataset_scaled.shape[0],:]
+        
+        self.train, self.test = train_test_split(self.dataset_scaled_short,train_size=0.7,test_size=0.3)
+        #print(self.train.shape)
+        #print(self.test.shape)
+        
+    
+    def function(self,x):
+        return self.f(x)
+    
+    def train_data(self):
+          return self.train
+      
+    def test_data(self):
+        return self.test
+    
+    def fit_model_plot_results(self,epochs,batch_size):
+         x=self.train[:,0:(self.train.shape[1]-1)]
+         y=self.train[:,self.train.shape[1]-1]
+
+         self.history=self.model.fit(x,
+                                     y,
+                                     epochs=epochs,
+                                     batch_size=batch_size)
+       
+         plt.plot(self.history.history['loss'])
+         plt.title('Model loss')
+         plt.ylabel('Loss')
+         plt.xlabel('Epoch')
+         plt.legend(['Train', 'Test'], loc='upper left')
+         plt.show()
+
+         self.plot_test_results(batch_size)
+         self.plot_out_of_range_results(batch_size)
+         
+   
+    def plot_test_results(self,batch_size):
+         x=self.test[:,0:(self.test.shape[1]-1)]
+       
+         y_predict_test=self.model.predict(x,batch_size=batch_size)
+         y_predict_test.shape = (len(y_predict_test))
+         
+         dataset_invert = np.copy(self.test)
+         dataset_invert[:,dataset_invert.shape[1]-1]=y_predict_test
+         y_predict_test= self.scaler.inverse_transform(dataset_invert)[:,dataset_invert.shape[1]-1]
+         
+         test_tf=self.scaler.inverse_transform(self.test)
+         x=test_tf[:,0]
+         y=test_tf[:,test_tf.shape[1]-1]
+         
+         plt.scatter(x,y)
+         plt.scatter(x,y_predict_test)
+         plt.show()
+
+    def plot_out_of_range_results(self,batch_size):
+        
+         x=self.dataset_out_of_range[:,0:(self.dataset_out_of_range.shape[1]-1)]
+         
+         y_predict_out_range=self.model.predict(x,batch_size=batch_size)
+         y_predict_out_range.shape = (len(y_predict_out_range))
+         
+         dataset_invert = np.copy(self.dataset_out_of_range)
+         dataset_invert[:,dataset_invert.shape[1]-1]=y_predict_out_range
+         y_predict_out_range= self.scaler.inverse_transform(dataset_invert)[:,dataset_invert.shape[1]-1]
+         
+         test_tf=self.scaler.inverse_transform(self.dataset_out_of_range)
+         x=test_tf[:,0]
+         y=test_tf[:,test_tf.shape[1]-1]
+         
+         plt.scatter(x,y)
+         plt.scatter(x,y_predict_out_range)
+         plt.show()
+
+    
+    def test_out_of_range(self,range_):
+        test_out=np.random.random(n)*(upper+range_)
+        
+        
+        
+        
+        
+    
+    
+    
+
+def train_x_y_model(dataset,model,lstm=0,epochs=10,batch_size=1):
+
+    #model.compile(loss='mean_squared_error', optimizer='adam',metrics=['mae','acc'])
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    dataset = scaler.fit_transform(dataset)
+    train_size = round(len(dataset)*0.7)
+    test_size = len(dataset) - train_size
+    train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+    print(len(train), len(test))
+    
+    x=train[:,0:(train.shape[1]-1)]
+    y=train[:,train.shape[1]-1]
+    x_test=test[:,0:(test.shape[1]-1)]
+    y_test=test[:,test.shape[1]-1]
+    x.shape=(train_size,train.shape[1]-1)
+    y.shape=(train_size,1)
+    x_test.shape=(test_size,test.shape[1]-1)
+    y_test.shape=(test_size,1)
+    x1=0
+    x_test1=0
+    if(lstm==1):
+        x1=x
+        x_test1=x_test
+        x = x[0:train_size].reshape((train_size, 1, 1))
+        x_test=x_test[0:test_size].reshape((test_size, 1, 1))
+        
+   
+    dataset_after = scaler.inverse_transform(dataset)
+
+    y_predict_train= model.predict(x,batch_size=batch_size)
+    y_predict_test= model.predict(x_test,batch_size=batch_size)
+
+    y_predict_train.shape = (len(y_predict_train))
+    dataset1 = np.copy(dataset_after[0:train_size,:])
+    dataset1[:,dataset1.shape[1]-1]=y_predict_train
+    y_predict_train= scaler.inverse_transform(dataset1)[:,dataset1.shape[1]-1]
+    
+    train, test = dataset_after[0:train_size,:], dataset_after[train_size:len(dataset),:]
+    print(len(train), len(test))
+    
+    x=train[:,0:(train.shape[1]-1)]
+    y=train[:,train.shape[1]-1]
+    x_test=test[:,0:(test.shape[1]-1)]
+    y_test=test[:,test.shape[1]-1]
+    x.shape=(train_size,train.shape[1]-1)
+    y.shape=(train_size,1)
+    x_test.shape=(test_size,test.shape[1]-1)
+    y_test.shape=(test_size,1)
+    x1=0
+    x_test1=0
+    if(lstm==1):
+        x1=x
+        x_test1=x_test
+        x = x[0:train_size].reshape((train_size, 1, 1))
+        x_test=x_test[0:test_size].reshape((test_size, 1, 1))
+        
+    
+    if(lstm==1):
+        plt.scatter(x1,y)
+        plt.scatter(x1,y_predict_train)
+    else:
+        plt.scatter(x,y)
+        plt.scatter(x,y_predict_train)
+   
+    plt.show()
+    
+    y_predict_test.shape = (len(y_predict_test))
+    dataset1 = np.copy(dataset[0:test_size,:])
+    dataset1[:,dataset1.shape[1]-1]=y_predict_test
+    y_predict_test= scaler.inverse_transform(dataset1)[:,dataset1.shape[1]-1]
+    
+    
+    if(lstm==1):
+        plt.scatter(x_test1,y_test)
+        plt.scatter(x_test1,y_predict_test)
+    else:
+        
+        plt.scatter(x_test,y_test)
+        plt.scatter(x_test,y_predict_test)
+   
+    plt.show()
+   
+    #plt.plot(m.history['acc'])
+    #plt.plot(m.history['val_acc'])
+    #plt.title('Model accuracy')
+    #plt.ylabel('Accuracy')
+    #plt.xlabel('Epoch')
+    #plt.legend(['Train', 'Test'], loc='upper left')
+    #plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(m.history['loss'])
+    #plt.plot(m.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+    return model
---
+++ test_networks.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  4 12:31:45 2019
+
+@author: joph
+"""
+
+from ml_classes import *
+
+#https://en.wikipedia.org/wiki/Universal_approximation_theorem
+
+x = np.random.random(10000)*10
+y1 = x 
+y2 = x*x
+y3 = np.sin(x)
+
+lags=100
+
+lagged=np.zeros((len(x)-lags,lags))
+
+for i in range(0,len(x)-lags):
+    lagged[i,:]=x[i:i+lags]
+    
+
+model1= sm.OLS(y1, x).fit()
+model2= sm.OLS(y2, x).fit()
+model3= sm.OLS(y3, x).fit()
+model4= sm.OLS(y3[0:(len(x)-lags)], lagged).fit()
+
+
+############model1
+fig = sm.graphics.plot_fit(model1,0)
+
+fig = sm.graphics.plot_fit(model2,0)
+
+fig = sm.graphics.plot_fit(model3,0)
+
+fig= sm.graphics.plot_fit(model4,0)
+
+
+model = Sequential()
+model.add(Dense(20 , activation='tanh',input_dim=1))
+model.add(LeakyReLU(alpha=0.03))
+model.add(Dense(20,activation='tanh'))
+model.add(LeakyReLU(alpha=0.03))
+model.add(Dense(1,activation='tanh'))
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['accuracy'])
+
+def linear_f(x):
+    return x
+
+def quadratic_f(x):
+    return x*x
+
+def sin_f(x):
+    return np.sin(x)
+
+n=20000
+batch_size=5
+max_elem=10000
+
+from ml_classes import *
+
+#linear=Function_data_no_ts(n,10,max_elem,model,linear_f,1)
+#linear.fit_model_plot_results(epochs,batch_size)
+
+#linear=Function_data_no_ts(n,10,max_elem,model,quadratic_f,1)
+#linear.fit_model_plot_results(epochs,batch_size)
+
+#linear=Function_data_no_ts(n,10,max_elem,model,sin_f,1)
+#linear.fit_model_plot_results(epochs,batch_size)
+
+lags=10
+epochs=10
+
+model = Sequential()
+model.add(Dense(20, activation='tanh',input_dim=lags))
+model.add(LeakyReLU(alpha=0.03))
+model.add(Dense(20,activation='tanh'))
+model.add(LeakyReLU(alpha=0.03))
+model.add(Dense(1,activation='tanh'))
+model.compile(optimizer='adam',
+              loss='mean_squared_error',
+              metrics=['accuracy'])
+
+linear=Function_data_no_ts(n,10,max_elem,model,linear_f,lags)
+linear.fit_model_plot_results(epochs,batch_size)
+
+linear=Function_data_no_ts(n,10,max_elem,model,quadratic_f,lags)
+linear.fit_model_plot_results(epochs,batch_size)
+
+linear=Function_data_no_ts(n,10,max_elem,model,sin_f,lags)
+linear.fit_model_plot_results(epochs,batch_size)
+
+model = Sequential()
+model.add(LSTM(30, batch_input_shape=(1, 1, 1), return_sequences=True, stateful=True))
+model.add(LSTM(30, return_sequences=False, stateful=True))
+model.add(Dense(30))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer=adam(lr=0.0001))
+
+linear=Function_data_no_ts(n,10,max_elem,model,linear_f,1)
+linear.fit_model_plot_results(epochs,batch_size)
+
+linear=Function_data_no_ts(n,10,max_elem,model,quadratic_f,1)
+linear.fit_model_plot_results(epochs,batch_size)
+
+linear=Function_data_no_ts(n,10,max_elem,model,sin_f,1)
+linear.fit_model_plot_results(epochs,batch_size)
+
+
+
+
+
+#model = Sequential()
+#model.add(LSTM(4, input_shape=(1, look_back)))
+#model.add(Dense(1))
+#model.compile(loss='mean_squared_error', optimizer='adam')
+#model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+
+# make predictions
+trainPredict = model.predict(trainX)
+#testPredict = model.predict(testX)
+# invert predictions
+#trainPredict = scaler.inverse_transform(trainPredict)
+#trainY = scaler.inverse_transform([trainY])
+#testPredict = scaler.inverse_transform(testPredict)
+#testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+#trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+#print('Train Score: %.2f RMSE' % (trainScore))
+#testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+#print('Test Score: %.2f RMSE' % (testScore))
+#plt.scatter(x,y1)
+plt.scatter(x,y3)
+plt.plot(x,y3)
+
+
